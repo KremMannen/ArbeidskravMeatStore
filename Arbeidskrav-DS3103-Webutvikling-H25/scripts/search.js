260a0721a9d9d7783d4e01093d6a9f7132416387
KremMannen <flyingnorwegians@gmail.com>
2025-09-14 14:36:38 +0200

    forgot to update search site

--- Arbeidskrav-DS3103-Webutvikling-H25/scripts/search.js
+++ Arbeidskrav-DS3103-Webutvikling-H25/scripts/search.js
@@ -1,41 +1,48 @@
 import htmlGenerator from "./modules/html-generator.js";
 import searchHandler from "./modules/search-handler.js";
 
-const resultSection = document.querySelector(".result-section");
-const searchButton = document.querySelector(".header-banner__search-toggle");
-let searchField = document.querySelector(".header-banner__search-field");
+setTimeout(() => {
+  const resultSection = document.querySelector(".result-section");
+  const searchButton = document.querySelector(".header-banner__search-toggle");
+  let searchField = document.querySelector(".header-banner__search-field");
 
-const showSearchResults = (element) => {
-  if (!element) {
-    return;
+  const showSearchResults = (element) => {
+    if (!element) {
+      return;
+    }
+    const searchResults = searchHandler.getSearchResults();
+    let htmlTxt = "";
+    searchResults.forEach((product) => {
+      htmlTxt += htmlGenerator(product);
+    });
+    element.innerHTML = htmlTxt;
+  };
+
+  function search() {
+    const searchTerm = searchField.value.trim();
+    searchField.value = "";
+
+    if (searchTerm) {
+      searchHandler.executeSearch(searchTerm);
+    }
+    searchField.classList.add("hidden");
   }
-  const searchResults = searchHandler.getSearchResults();
-  let htmlTxt = "";
-  searchResults.forEach((product) => {
-    htmlTxt += htmlGenerator(product);
+
+  // Initialize
+  showSearchResults(resultSection);
+
+  searchButton.addEventListener("click", () => {
+    searchHandler.toggleSearchField(searchField);
+
+    // Sets cursor on the typing field so user can type immediately
+    if (!searchField.classList.contains("hidden")) {
+      setTimeout(() => searchField.focus(), 100);
+    }
   });
-  element.innerHTML = htmlTxt;
-};
 
-function search() {
-  const searchTerm = searchField.value.trim();
-  searchField.value = "";
-
-  if (searchTerm) {
-    searchHandler.executeSearch(searchTerm);
-  }
-  searchField.classList.add("hidden");
-}
-
-// Initialize
-showSearchResults(resultSection);
-
-searchButton.addEventListener("click", () => {
-  searchHandler.toggleSearchField(searchField);
-});
-
-searchField.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    search();
-  }
-});
+  searchField.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  });
+}, 10); // 10 ms
