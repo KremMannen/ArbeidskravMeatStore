385602ef53f00d77a96874e8225aac635e2c2f9d
mariusaellingsen <marius.a.ellingsen@gmail.com>
2025-09-14 15:05:12 +0200

    changes to ad banners

---
+++ Arbeidskrav-DS3103-Webutvikling-H25/scripts/modules/ad-banners.js
@@ -0,0 +1,16 @@
+const Banners = (() => {
+    const ads = [
+        {
+            imgName:"kotelettbanner.jpg",
+            description:""
+        },
+        {
+            imgName:"pølsebanner.jpg",
+            description:""
+        },
+         {
+            imgName:"biffbanner.jpg",
+            description:""
+        }
+    ]
+})
