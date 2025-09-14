dd139c55b757d8dd2afee05b63be986c6f8b570f
KremMannen <flyingnorwegians@gmail.com>
2025-09-14 14:34:12 +0200

    fixed bug on button!!

--- Arbeidskrav-DS3103-Webutvikling-H25/scripts/index.js
+++ Arbeidskrav-DS3103-Webutvikling-H25/scripts/index.js
@@ -2,40 +2,48 @@ import inventory from "./modules/inventory.js";
 import htmlGenerator from "./modules/html-generator.js";
 import searchHandler from "./modules/search-handler.js";
 
-let productSection = document.querySelector(".products-section");
-let searchButton = document.querySelector(".header-banner__search-toggle");
-let searchField = document.querySelector(".header-banner__search-field");
+// Small delay to ensure everything is ready after navigation. This prevents buttons from buggin out.
+setTimeout(() => {
+  let productSection = document.querySelector(".products-section");
+  let searchButton = document.querySelector(".header-banner__search-toggle");
+  let searchField = document.querySelector(".header-banner__search-field");
 
-// functions
-const showAllProducts = (element) => {
-  const allProducts = inventory.getAll();
-  let htmlTxt = "";
+  // Functions
+  const showAllProducts = (element) => {
+    const allProducts = inventory.getAll();
+    let htmlTxt = "";
 
-  allProducts.forEach((product) => {
-    htmlTxt += htmlGenerator(product);
+    allProducts.forEach((product) => {
+      htmlTxt += htmlGenerator(product);
+    });
+    element.innerHTML = htmlTxt;
+  };
+
+  function search() {
+    const searchTerm = searchField.value.trim();
+    searchField.value = "";
+
+    if (searchTerm) {
+      searchHandler.executeSearch(searchTerm);
+    }
+    searchField.classList.add("hidden");
+  }
+
+  // Initialize
+  showAllProducts(productSection);
+
+  searchButton.addEventListener("click", () => {
+    searchHandler.toggleSearchField(searchField);
+
+    // Sets cursor on the typing field so user can type immediately
+    if (!searchField.classList.contains("hidden")) {
+      setTimeout(() => searchField.focus(), 100);
+    }
   });
-  element.innerHTML = htmlTxt;
-};
 
-function search() {
-  const searchTerm = searchField.value.trim();
-  searchField.value = "";
-
-  if (searchTerm) {
-    searchHandler.executeSearch(searchTerm);
-  }
-  searchField.classList.add("hidden");
-}
-
-// Initialize
-showAllProducts(productSection);
-
-searchButton.addEventListener("click", () => {
-  searchHandler.toggleSearchField(searchField);
-});
-
-searchField.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    search();
-  }
-});
+  searchField.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  });
+}, 10); // 10 ms
